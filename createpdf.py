07255d86e128eb381fd991f1769a415ffa7de9cd
Huzaifa-fh <huzaifabinfaheem1@gmail.com>
2023-12-14 23:02:04 +0500

    Increased Complexity of Random Clean PDFs

--- createpdf.py
+++ createpdf.py
@@ -4,9 +4,9 @@ from reportlab.lib.pagesizes import letter
 import random
 import string
 import textwrap
+from PIL import Image
 
-def generate_random_paragraphs(num_paragraphs=5):
-    # Generate a number of random paragraphs for the PDF page
+def generate_random_paragraphs(num_paragraphs=50):
     paragraphs = []
     for _ in range(num_paragraphs):
         words = ''.join(random.choices(string.ascii_letters + ' '*10, k=500)).split(' ')
@@ -14,38 +14,53 @@ def generate_random_paragraphs(num_paragraphs=5):
         paragraphs.append(paragraph)
     return paragraphs
 
-def create_pdf(file_path):
-    # Create a PDF with 1 page of random content
+def generate_simple_image():
+    # Generate a simple colored rectangle image
+    img = Image.new('RGB', (200, 200), color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    img_path = f"temp_image_{random.randint(0, 999999)}.png"
+    img.save(img_path)
+    return img_path
+
+def insert_random_image(pdf, x, y):
+    img_path = generate_simple_image()
+    pdf.drawImage(img_path, x, y, width=200, height=200)
+    os.remove(img_path)  # Clean up the image file
+
+def create_pdf(file_path, num_pages):
     pdf = canvas.Canvas(file_path, pagesize=letter)
-    paragraphs = generate_random_paragraphs()
-    x = 50
-    y = 750
+    paragraphs = generate_random_paragraphs(num_pages * 5)
+    x, start_y = 50, 750
+    y = start_y
+
     for paragraph in paragraphs:
+        if y < 200:
+            insert_random_image(pdf, 100, y - 150)
+            pdf.showPage()
+            y = start_y
+
         lines = textwrap.wrap(paragraph, width=60)
         for line in lines:
+            if y < 50:
+                pdf.showPage()
+                y = start_y
             pdf.drawString(x, y, line)
-            y -= 15  # move to the next line
-        y -= 15  # extra space between paragraphs
+            y -= 15
+        y -= 15
+
+    insert_random_image(pdf, 100, y - 150)
     pdf.save()
 
-def generate_pdfs(output_directory, num_pdfs):
-    # Create the output directory if it doesn't exist
+def generate_pdfs(output_directory, num_pdfs, num_pages):
     os.makedirs(output_directory, exist_ok=True)
 
-    # Generate n PDFs with 1 page of random content each
     for i in range(1, num_pdfs + 1):
         pdf_filename = f"random_pdf_{i}.pdf"
         pdf_path = os.path.join(output_directory, pdf_filename)
-        create_pdf(pdf_path)
+        create_pdf(pdf_path, num_pages)
         print(f"Created PDF: {pdf_path}")
 
 if __name__ == "__main__":
-    # Specify the directory where the PDFs will be created
-    output_directory = "./cleanpdfs"
-
-    # Specify the number of PDFs to generate
-    num_pdfs = 10
-
-    # Generate the PDFs
-    generate_pdfs(output_directory, num_pdfs)
-
+    num_pages = int(input("Enter the number of pages for each PDF: "))
+    num_pdfs = int(input("Enter the number of PDFs to generate: "))
+    output_directory = input("Enter the output directory path: ")
+    generate_pdfs(output_directory, num_pdfs, num_pages)
